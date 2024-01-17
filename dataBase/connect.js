1f0ca5c4c85362a51b8477dce4f22b8c0c897703
Saicharan-Yadav <saicharanyadav.p20@iiits.in>
2024-01-17 17:43:14 +0530

    resolved

--- dataBase/connect.js
+++ dataBase/connect.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const db = "mongodb+srv://saiCharan:test123@oneshot.idiwbja.mongodb.net/";
+const db = process.env.db_url;
 
 const connectDB = async () => {
   try {
